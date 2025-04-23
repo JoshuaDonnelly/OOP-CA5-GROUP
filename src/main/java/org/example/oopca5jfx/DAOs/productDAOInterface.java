43ea68293ef696d26dc0f12801a04004a1f5df36
josherbosher <thejokesters12@gmail.com>
2025-04-23 18:23:44 +0100

    Add files via upload

---
+++ src/main/java/org/example/oopca5jfx/DAOs/productDAOInterface.java
@@ -0,0 +1,20 @@
+package org.example.oopca5jfx.DAOs;
+
+import org.example.oopca5jfx.DTOs.productDTO;
+import java.util.List;
+
+public interface productDAOInterface {
+    List<productDTO> getAllProducts();
+    productDTO getProductById(int id);
+    boolean deleteProductById(int id);
+    productDTO insertProduct(String p);
+    boolean updateProduct(String p);
+    List<productDTO> searchProductsByKeyword(String keyword);
+
+
+    // New JSON methods
+    String getProductJsonById(int id);
+    String getAllProductsJson();
+    String getProductsJsonByKeyword(String keyword);
+
+}
